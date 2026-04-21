4242ff4a9c2ffd709b767ce7e5dfcd7297d20b5b
Gillian Hanley <gghanley04@gmail.com>
2026-04-21 17:18:12 -0400

    error in the proxy

--- src/proxy.js
+++ src/proxy.js
@@ -1,7 +1,5 @@
+export { auth as proxy } from "./auth-edge";
 
-export { auth as proxy } from "./auth"
-
-//Decide which routes to run proxy.js on
 export const config = {
   matcher: ["/add-profile", "/profile/:path*/edit"],
 };
